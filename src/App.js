1f2846c2239565d590c1ea76bba6107864a9a5ca
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2020-11-20 09:39:22 -0600

    main branch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import {Route, Link, Switch} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
