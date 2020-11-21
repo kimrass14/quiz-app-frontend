760f1788538cc12b3161fddaef1c8d11ba975bde
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2020-11-20 21:46:49 -0600

    multiple choice shuffle, render as buttons, onclick compare correct or incorrect

--- src/Components/MultipleChoice/MultipleChoice.js
+++ src/Components/MultipleChoice/MultipleChoice.js
@@ -2,9 +2,22 @@ import React, {useEffect, useState} from 'react'
 import './MultipleChoice.scss'
 
 const MultipleChoice = (props) => {
-    console.log('mult choice props', props)
+    // console.log('mult choice props', props)
 
     const [choices, setChoices] = useState([])
+
+    const handleCompare = (userAnswer) => {
+
+        if (userAnswer === questionObj.correct_answer) {
+            console.log("correct")
+        } else {
+            console.log("incorrect")
+        }
+
+        return(
+            <div></div>
+        )
+    }
 
     const questionObj = props.question
 
@@ -12,25 +25,35 @@ const MultipleChoice = (props) => {
         if (props.question) {
         let choices = []
         choices.push(questionObj.correct_answer, questionObj.incorrect_answer_1, questionObj.incorrect_answer_2, questionObj.incorrect_answer_3)
-        setChoices(choices)
+        const shuffledChoices = choices.sort(() => Math.random() - 0.5)
+        setChoices(shuffledChoices)
         }
     }, [props.question])
-    
-    console.log('choices', choices)
 
     //need to shuffle choices array, then map over shuffled array
+    // const shuffledChoices = choices.sort(() => Math.random() - 0.5)
+    // console.log('shuffledChoices', shuffledChoices)
 
     const multipleChoice = choices.map((choice, index) => {
         return(
-            <div>{choice}</div>
+            <input 
+                key={index}
+                type="button" 
+                value={choice}
+                onClick={() => {handleCompare(choice)}}
+            />
         )
     })
-
 
     return(
         <div className="multiple-choice">
             <div>MultipleChoice</div>
-            <div>{multipleChoice}</div>
+            {multipleChoice}
+            {/* <input
+                type="button"
+                value={choices[0]}
+                onClick={handleCompare}
+            /> */}
         </div>
     )
 }
