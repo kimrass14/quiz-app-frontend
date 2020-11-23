bf56994aa7cbe318bc9e53729c0fbbc8f9700d38
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2020-11-23 00:03:26 -0600

    circle elements forced to center of g element and dont collide

--- src/Components/BubbleChart/BubbleChart.js
+++ src/Components/BubbleChart/BubbleChart.js
@@ -12,31 +12,63 @@ const BubbleChart = () => {
     //     {"category": "General", "correct": 8},
     // ]
 
-    const dataSet = [5,9,10,14,20]
+    const dataSet = [
+        {"id": 1, "num": 5},
+        {"id": 2, "num": 9},
+        {"id": 3, "num": 15},
+        {"id": 4, "num": 10},
+        {"id": 5, "num": 25},
+    ]
 
     // const [dataSet, setDataSet] = useState(score)
     // console.log('dataset', dataSet)
     const gRef = useRef()
 
-    useEffect(() => {
-        ready()
-    }, [dataSet])
+    // useEffect(() => {
+    //     ready()
+    // }, [dataSet])
 
-    //d3.queue().defer(d3.csv, "sales.csv").await(ready)
+    const radiusScale = d3.scaleSqrt().domain([])
+
+    //forced to x and y
+    const simulation = d3.forceSimulation()
+        .force("x", d3.forceX(250 /2).strength(.05))
+        .force("y", d3.forceY(250 /2).strength(.05))
+        .force("collide", d3.forceCollide(10))
+
 
     const ready = () => {
 
-    const circles = d3
-        .select(gRef.current)
-        .selectAll('numbers')
-        .data(dataSet, (d) => d)
-        .enter().append("circle")
-        .attr("class", "category")
-        .attr("r", 10)
-        .attr("fill", "blue")
-    console.log('circles', circles)
+        //binding dataset to circle elements created within the g element
+        const circles = d3
+            .select(gRef.current)
+            .selectAll('circles')
+            .data(dataSet)
+            .enter().append("circle")
+            .attr("class", "category")
+            .attr("r", 10)
+            .attr("fill", "blue")
+            .attr("cx", 125)
+            .attr("cy", 125)
+        console.log('circles', circles)
+
+        //tick function for simulations. pretty standard.
+        const ticked = () => {
+            circles
+                .attr("cx", function(d) {
+                    return d.x
+                })
+                .attr("cy", function(d) {
+                    return d.y
+                })
+        }
+
+        //at every tick of clock, run ticked function
+        simulation.nodes(dataSet)
+            .on('tick', ticked)
 
     }
+    ready()
 
     return(
         <div className="container">
