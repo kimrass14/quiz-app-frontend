9e209434d232368da943cddf7dd1d7aeec1d67dd
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2020-11-25 14:03:36 -0600

    DOES NOT BREAK when there are no questions added for a category or click next and all questions have been answered correctly. SUCCESS

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -9,34 +9,25 @@ const Quiz = (props) => {
         const incorrectQsArr = props.selectedCatAndQuestions.questions.filter(question => question.user_answer !== "correct")
         const shuffledIncorrectQs = incorrectQsArr.sort(() => Math.random() - 0.5)
         console.log('shuffled', shuffledIncorrectQs)
-        
-        // useEffect(() => {
-        //     setQuestion(shuffledIncorrectQs)
-        // },[])
 
         return(
             <>
-                <div>{shuffledIncorrectQs[0].quiz_question}</div>
+                {shuffledIncorrectQs.length > 0 ? <div>{shuffledIncorrectQs[0].quiz_question}</div> : <div>You answered all questions correctly!</div>}
                 <MultipleChoice question={shuffledIncorrectQs[0]} url={props.url} getCategories={props.getCategories} addCount={props.addCount}/>
             </>
         )
     }
     
-    const noMoreIncorrect = "You answered all questions correctly!"
+    const noQuestions = "Add questions to this category"
 
     return(
         <div className="quiz">
             <div>Quiz</div>
-            {props.selectedCatAndQuestions.questions && props.selectedCatAndQuestions.questions.length > 0 ? loaded() : noMoreIncorrect}
+            {props.selectedCatAndQuestions.questions && props.selectedCatAndQuestions.questions.length > 0 ? loaded() : noQuestions}
             
             <button onClick={() => {
                 props.handleGetCatQuestions(props.selectedCatAndQuestions)}}>Next
             </button>
-            {/* <button onClick={() => {
-                props.handleGetCatQuestions(props.selectedCatAndQuestions)
-                props.history.push('/q')}}>
-                Next
-            </button> */}
         </div>
     )
 }
